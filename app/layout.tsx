3c7c3f08cde6d603f445f9b5b3ff051f2d0847d2
MD Tanvir <mdtanvir4679876@gmail.com>
2026-06-11 02:18:48 +0600

    Initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,23 @@
+import { ClerkProvider, SignInButton, SignUpButton, UserButton, SignedIn, SignedOut } from '@clerk/nextjs'
+import './globals.css'
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body>
+          <header className="flex justify-end p-4 gap-4">
+            <SignedOut>
+              <SignInButton />
+              <SignUpButton />
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+          </header>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
+  )
+}
